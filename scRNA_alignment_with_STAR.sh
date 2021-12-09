8ce39bf062b7c967756425aefb2c3acb2382bff6
AristoQian <92309318+AristoQian@users.noreply.github.com>
2021-12-09 01:35:47 -0600

    Update scRNA_alignment_with_STAR.sh

--- scRNA_alignment_with_STAR.sh
+++ scRNA_alignment_with_STAR.sh
@@ -7,9 +7,12 @@
 #SBATCH --mem=80gb
 #SBATCH --job -name="opt5_chenxing"
 
+# above lines are SBATCH options in Quest (Northwestern University)
+
 cd $SLURM_SUBMIT_DIR
 module purge all
 module load STAR/2.7.9a
 
+#in the same line
 STAR --runThreadN 20 --soloType CB_UMI_Simple --soloCBwhitelist ./737K-august-2016.txt --genomeDir /projects/e31265/humanref --outFilterType BySJout --alignIntronMax 100000 --quantMode GeneCounts --outSAMtype None  --soloCellFilter EmptyDrops_CR --soloFeatures Gene --readFilesPrefix /projects/e31265/marrow/ --readFilesCommand "gzip - cd" --readFilesIn marrow_S1_L001_R2_001.fastq.gz,marrow_S1_L002_R2_001.fastq.gz,marrow_S1_L003_R2_001.fastq.gz,marrow_S1_L004_R2_001.fastq.gz,marrow_S1_L005_R2_001.fastq.gz,marrow_S1_L006_R2_001.fastq.gz,marrow_S1_L007_R2_001.fastq.gz,marrow_S1_L008_R2_001.fastq.gz marrow_S1_L001_R1_001.fastq.gz,marrow_S1_L002_R1_001.fastq.gz,
 marrow_S1_L003_R1_001.fastq.gz,marrow_S1_L004_R1_001.fastq.gz,marrow_S1_L005_R1_001.fastq.gz,marrow_S1_L006_R1_001.fastq.gz,marrow_S1_L007_R1_001.fastq.gz,marrow_S1_L008_R1_001.fastq.gz
