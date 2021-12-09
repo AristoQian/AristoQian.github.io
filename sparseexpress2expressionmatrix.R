6e1be80f6cb434c5b39cdc1fc707ce443545876f
AristoQian <92309318+AristoQian@users.noreply.github.com>
2021-12-09 00:49:40 -0600

    Add files via upload

---
+++ sparseexpress2expressionmatrix.R
@@ -0,0 +1,14 @@
+library(Matrix)
+matrix_dir = "~/filtered_feature_bc_matrix/hg19/"   
+barcode.path <- paste0(matrix_dir, "barcodes.tsv")
+features.path <- paste0(matrix_dir, "genes.tsv")
+matrix.path <- paste0(matrix_dir, "matrix.mtx")
+mat <- readMM(file = matrix.path)
+feature.names = read.delim(features.path,
+                           header = FALSE,
+                           stringsAsFactors = FALSE)
+barcode.names = read.delim(barcode.path,
+                           header = FALSE,
+                           stringsAsFactors = FALSE)
+colnames(mat) = barcode.names$V1
+rownames(mat) = feature.names$V1
